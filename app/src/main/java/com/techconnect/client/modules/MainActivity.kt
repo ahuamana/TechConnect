fdb606d65995737ce3e538c772538a66c6e1cde3
ahuamana <73962071@continental.edu.pe>
2023-05-28 00:19:53 -0500

    if applied this will add contect preview in login

--- app/src/main/java/com/techconnect/client/modules/MainActivity.kt
+++ app/src/main/java/com/techconnect/client/modules/MainActivity.kt
@@ -10,21 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.techconnect.client.modules.screens.LoginScreen
 import com.techconnect.client.ui.theme.TechConnectTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TechConnectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                LoginScreen()
             }
         }
     }
